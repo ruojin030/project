fd7e823811acce55aec680cabb8b563efa69c0d5
张若瑾 <ruojin@zhangrujindeMBP.home>
2019-03-24 23:58:01 -0400

    try

--- search/search.js
+++ search/search.js
@@ -5,7 +5,7 @@ const mongo_address = 'mongodb://130.245.171.133:27017';
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json()
 
-const port = 3001
+const port = 80
 
 app.get('/',function(req,res){
     res.send("hello!")

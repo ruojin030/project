a8c438d5a799d229de4eb7fda43ff76ab058a373
Ruojin Zhang <ruojin030@gmail.com>
2019-05-09 01:53:30 -0400

    haode

--- esquestions/routers/questions.js
+++ esquestions/routers/questions.js
@@ -38,7 +38,7 @@ router.post('/add', jsonParser, function (req, res) {
                 } else {
                     data['has_media'] = true
                 }
-                data['id'] = uniqid().toString;
+                data['id'] = uniqid()
                 data['user'] = req.cookies.session.current_user
                 data['title'] = req.body.title
                 data['body'] = req.body.body
@@ -75,8 +75,12 @@ router.post('/add', jsonParser, function (req, res) {
                             for (i in req.body.media) {
                                 db.collection("medias").updateOne({ "id": req.body.media[i] }, { $set: { "used": true } })
                             }
-                            es.index({index:esindex,body:data,id:data.id})
-                            db.collection('questions').insertOne(data)
+                            
+                            db.collection('questions').insertOne(data,function(err){
+                                delete data.id
+                                delete data._id
+                                es.index({index:esindex,body:data,id:data.id})
+                            })
                             console.log(data['id'] +" add success by "+ req.cookies.session.current_user)
                             res.json({ 'status': "OK", 'id': data.id })
                         }

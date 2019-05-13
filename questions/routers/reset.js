914f15ff98daac6fcbd90834eb7d3af81b3d9714
Ruojin Zhang <ruojin030@gmail.com>
2019-05-12 20:23:49 -0400

    reset

--- questions/routers/reset.js
+++ questions/routers/reset.js
@@ -3,6 +3,9 @@ var router = express.Router();
 router.get('/',function(req,res){
     console.log("reset success")
     var db = req.app.locals.db
+    var mdeia = req.app.locals.media_db
+    media_db.collection('users').remove({})
+    media_db.collection('medias').remove({})
     db.collection('answers').remove({},function(err,result){
         db.collection('questions').remove({},function(err,result){
             db.collection('users').remove({},function (err, result){
